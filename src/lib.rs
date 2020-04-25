2ce72d514e093afae1172b739686c3dfc22792e1
Nick Cuthbert <nick@cuthbert.co.za>
2020-04-25 21:24:43 +0200

    Got console.log working!

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,56 @@
 use rusty_v8 as v8;
 use std::convert::TryFrom;
+use std::println;
 
 struct ActorDefintion {
     name: String,
-    sourceCode: String,
+    source_code: String,
     version: i32,
 }
 
-const sourceCode: &str = "
+const SOURCE_CODE: &str = "
     function main(state, msg, ctx) {     
+        console.log(\"Hello World!\");
         return state + msg;
     }
 ";
+
+fn print(
+    scope: v8::FunctionCallbackScope,
+    args: v8::FunctionCallbackArguments,
+    _rv: v8::ReturnValue,
+) -> () {
+    let arg_len = args.length();
+    assert!(arg_len >= 0 && arg_len <= 2);
+    let obj = args.get(0);
+    let is_err_arg = args.get(1);
+    let mut hs = v8::HandleScope::new(scope);
+    let scope = hs.enter();
+    let mut is_err = false;
+    if arg_len == 2 {
+        let int_val = is_err_arg
+            .integer_value(scope)
+            .expect("Unable to convert to integer");
+        is_err = int_val != 0;
+    };
+    let mut try_catch = v8::TryCatch::new(scope);
+    let _tc = try_catch.enter();
+    let str_ = match obj.to_string(scope) {
+        Some(s) => s,
+        None => v8::String::new(scope, "").unwrap(),
+    };
+    if is_err {
+        eprint!(
+            "Hello err from js to rust: {}\n",
+            str_.to_rust_string_lossy(scope)
+        );
+    } else {
+        print!(
+            "Hello from js to rust: {}\n",
+            str_.to_rust_string_lossy(scope)
+        );
+    }
+}
 
 pub fn run_v8() {
     let platform = v8::new_default_platform();
@@ -21,7 +60,6 @@ pub fn run_v8() {
     let mut create_params = v8::Isolate::create_params();
     create_params.set_array_buffer_allocator(v8::new_default_allocator());
     let mut isolate = v8::Isolate::new(create_params);
-
     let mut handle_scope = v8::HandleScope::new(&mut isolate);
     let scope = handle_scope.enter();
 
@@ -29,12 +67,28 @@ pub fn run_v8() {
     let mut context_scope = v8::ContextScope::new(scope, context);
     let scope = context_scope.enter();
 
-    let code = v8::String::new(scope, sourceCode).unwrap();
+    let global = context.global(scope);
+    let console = v8::Object::new(scope);
+    global.set(
+        context,
+        v8::String::new(scope, "console").unwrap().into(),
+        console.into(),
+    );
+    let mut print_tmpl: v8::Local<v8::FunctionTemplate> = v8::FunctionTemplate::new(scope, print);
+    let print_val = print_tmpl.get_function(scope, context).unwrap();
+    console.set(
+        context,
+        v8::String::new(scope, "log").unwrap().into(),
+        print_val.into(),
+    );
+
+    let code = v8::String::new(scope, SOURCE_CODE).unwrap();
     println!("javascript code: {}", code.to_rust_string_lossy(scope));
     let mut script = v8::Script::compile(scope, context, code, None).unwrap();
     script.run(scope, context).unwrap();
     let function_name = v8::String::new(scope, "main").unwrap();
     let global = context.global(scope);
+
     let main = global
         .get(scope, context, v8::Local::from(function_name))
         .unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use actix_web::{middleware, web, App, HttpServer};
+// use actix_web::{middleware, web, App, HttpServer};
 use golem;
-use golem::controllers;
+// use golem::controllers;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+fn main() {
+    // // // HttpServer::new(|| {
+    // // //     App::new()
+    // // //         .wrap(middleware::Compress::default())
+    // // //         .service(controllers::index)
+    // // // })
+    // // // .bind("127.0.0.1:8080")?
+    // // // .run()
+    // // // .await
+    // ()
     golem::run_v8();
-    HttpServer::new(|| {
-        App::new()
-            .wrap(middleware::Compress::default())
-            .service(controllers::index)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
 }
