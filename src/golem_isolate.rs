f16166c7b1921283b516b5934aa606189213c7f4
Nick Cuthbert <nick@cuthbert.co.za>
2020-05-10 19:29:42 +0200

    Got snapshotting and execution working with isolate core

--- src/golem_isolate.rs
+++ src/golem_isolate.rs
@@ -1,17 +1,21 @@
 use std::mem;
-// use crate::isolate_core::{IsolateCore, StartupData as CoreStartupData, Script, Snapshot};
-// use rusty_v8::{self as v8, Function, Global, Local};
+
 use crate::golem_isolate::IsolateCreationError::{FailedToCompileCode, NoMain};
 use std::convert::TryFrom;
 use std::borrow::BorrowMut;
-use std::sync::Arc;
-use deno_core::{Script, Snapshot, CoreIsolate};
-use rusty_v8::{Function, Global, Local, ContextScope, HandleScope};
+use std::sync::{Arc, Mutex};
+use deno_core::{Script, Snapshot, CoreIsolate, ErrBox};
+use rusty_v8::{self as v8, Function, Global, Local, ContextScope, HandleScope, Value};
 use deno_core::Snapshot::{JustCreated, Static};
 use futures::io::IoSlice;
 use std::ffi::CStr;
-use std::mem::transmute;
+use std::mem::{transmute, forget};
 use std::pin::Pin;
+use actix_web::web::scope;
+use std::future::Future;
+use futures::task::{Context, Poll};
+use futures::{TryFuture, FutureExt, TryFutureExt};
+
 
 pub enum IsolateCreationError {
     NoMain,
@@ -66,12 +70,58 @@ impl GolemSnapshot {
     }
 }
 
+trait Invokeable {
+    fn invoke_function(self: &mut Self, handle: &Global<Function>);
+}
+
+impl Invokeable for CoreIsolate {
+    fn invoke_function(self: &mut Self, handle: &Global<Function>) {
+        // let js_error_create_fn = &*self.js_error_create_fn;
+        let v8_isolate = self.v8_isolate.as_mut().unwrap();
+
+        let mut hs = v8::HandleScope::new(v8_isolate);
+        let scope = hs.enter();
+        assert!(!self.global_context.is_empty());
+        let context = self.global_context.get(scope).unwrap();
+        let mut cs = v8::ContextScope::new(scope, context);
+        let scope = cs.enter();
+
+
+        let mut try_catch = v8::TryCatch::new(scope);
+        let tc = try_catch.enter();
+
+        let arg1 = v8::Local::from(v8::Number::new(scope, 1.0));
+        let arg2 = v8::Local::from(v8::Number::new(scope, 2.0));
+
+        // let global = context.global(scope);
+
+        // // v8::Local::
+        // let main = global.get(scope, context, function_name);
+
+        let function: v8::Local<v8::Function> = handle.get(scope).unwrap();
+        // v8::Local::<v8::Function>::try_from(handle).unwrap();
+        // let main: v8::Local<v8::Function> = v8::Local::<v8::Function>::try_from(handle).unwrap();
+
+        // let global = context.global(scope);
+        let this = v8::Object::new(scope);
+        let result = function.call(scope, context, this.into(), &[arg1, arg2]).unwrap();
+        // let result = scope.escape(result);
+        // let result: Global<Value> = v8::Global::new_from(scope, result);
+
+        let str = result.to_string(scope).unwrap();
+        println!("Result is: {}", str.to_rust_string_lossy(scope));
+
+        // Result::Ok(result)
+    }
+}
+
 
 pub struct GolemIsolate {
-    core_isolate: Arc<Box<CoreIsolate>>,
-    main_handle: Option<Global<Function>>,
+    pub core_isolate: CoreIsolate,
+    main_handle: Global<Function>,
     cache_handle: Option<Global<Function>>,
     snapshot: Option<GolemSnapshot>,
+    state: Global<Value>,
 }
 
 
@@ -109,11 +159,12 @@ fn create_and_setup_isolate(startup_data: StartupData) -> Result<Box<CoreIsolate
     Ok(core_isolate)
 }
 
+
 impl GolemIsolate {
-    fn try_new(startup_data: StartupData) -> Result<Pin<Box<Self>>, IsolateCreationError> {
+    fn try_new(startup_data: StartupData) -> Result<Box<Self>, IsolateCreationError> {
         let (snapshot, startup_data) = match startup_data {
-            StartupData::Snapshot(_) => {
-                Ok((None, startup_data))
+            StartupData::Snapshot(snapshot) => {
+                Ok((None, StartupData::Snapshot(snapshot)))
             }
             StartupData::NativeSnapshot(_) => {
                 Ok((None, startup_data))
@@ -129,48 +180,19 @@ impl GolemIsolate {
 
         let mut core_isolate = create_and_setup_isolate(startup_data)?;
 
-        let main_handle = {
-            let mut v8_isolate = core_isolate.v8_isolate.as_mut().unwrap();
-            assert!(!core_isolate.global_context.is_empty());
-            println!("{}", "Unwrapped the isolate");
-
-
-            let mut hs = HandleScope::new(v8_isolate);
-            let scope = hs.enter();
-            assert!(!core_isolate.global_context.is_empty());
-
-            let context = core_isolate.global_context.get(scope).unwrap();
-
-            let mut cs = ContextScope::new(scope, context);
-            let scope = cs.enter();
-
-            let global = context.global(scope);
-
-            let main_function_name = rusty_v8::String::new(scope, "main").unwrap().into();
-            let main = global.get(scope, context, main_function_name);
-
-            let main = main
-                .and_then(|main| if main.is_function() { Some(main) } else { None })
-                .map(|main| Local::<Function>::try_from(main).unwrap());
-
-
-            match main {
-                Some(main) => {
-                    let mut m = Global::<Function>::new();
-                    m.set(scope, main);
-                    Ok(m)
-                }
-                None => Err(IsolateCreationError::NoMain),
-            }
+        let main_handle = Self::try_get_function_handle(&mut core_isolate, "main");
+        let main_handle = match main_handle {
+            Some(x) => Ok(x),
+            None => Err(NoMain)
         }?;
-        let main_handle = Some(main_handle);
-        // let main_handle = None;
+        println!("{}", "Got the main handle: true");
 
-        println!("Got the main handle {}", main_handle.is_some());
 
-        // let cache_handle = None;
+        let cache_handle = Self::try_get_function_handle(&mut core_isolate, "cache");
 
-        let cache_handle = {
+        println!("Got the cache handle: {}", cache_handle.is_some());
+
+        let state: Global<Value> = {
             let mut v8_isolate = core_isolate.v8_isolate.as_mut().unwrap();
             assert!(!core_isolate.global_context.is_empty());
             println!("{}", "Unwrapped the isolate");
@@ -178,38 +200,47 @@ impl GolemIsolate {
 
             let mut hs = HandleScope::new(v8_isolate);
             let scope = hs.enter();
-            assert!(!core_isolate.global_context.is_empty());
-
-            let context = core_isolate.global_context.get(scope).unwrap();
-
-            let mut cs = ContextScope::new(scope, context);
-            let scope = cs.enter();
-
-            let context = core_isolate.global_context.get(scope).unwrap();
-            let mut cs = ContextScope::new(scope, context);
-            let scope = cs.enter();
-            let global = context.global(scope);
-
-            let cache_function_name = rusty_v8::String::new(scope, "cache").unwrap().into();
-            let cache_function = global.get(scope, context, cache_function_name);
-            cache_function
-                .and_then(|cache_function| if cache_function.is_function() { Some(cache_function) } else { None })
-                .map(|cache_function| Local::<Function>::try_from(cache_function).unwrap())
-                .map(|cache_function: Local<Function>| Global::new_from(scope, cache_function))
+            let value: Local<Value> = v8::undefined(scope).into();
+            Global::new_from(scope, value)
         };
 
-        println!("Got the cache handle {}", cache_handle.is_some());
-
-        core_isolate.execute("test.js", " ");
 
         let golem = Self {
-            core_isolate: Arc::new(core_isolate),
+            core_isolate: *core_isolate,
             main_handle,
             cache_handle,
             snapshot,
+            state,
         };
 
-        Ok(Box::pin(golem))
+        Ok(Box::from(golem))
+    }
+
+    fn try_get_function_handle(core_isolate: &mut CoreIsolate, name: &str) -> Option<Global<Function>> {
+        let mut v8_isolate = core_isolate.v8_isolate.as_mut().unwrap();
+        assert!(!core_isolate.global_context.is_empty());
+        println!("{}", "Unwrapped the isolate");
+
+        let mut hs = HandleScope::new(v8_isolate);
+        let scope = hs.enter();
+        assert!(!core_isolate.global_context.is_empty());
+
+        let context = core_isolate.global_context.get(scope).unwrap();
+
+        let mut cs = ContextScope::new(scope, context);
+        let scope = cs.enter();
+
+        let context = core_isolate.global_context.get(scope).unwrap();
+        let mut cs = ContextScope::new(scope, context);
+        let scope = cs.enter();
+        let global = context.global(scope);
+
+        let function_name = rusty_v8::String::new(scope, name).unwrap().into();
+        let function = global.get(scope, context, function_name);
+        function
+            .and_then(|function| if function.is_function() { Some(function) } else { None })
+            .map(|function| Local::<Function>::try_from(function).unwrap())
+            .map(|function: Local<Function>| Global::new_from(scope, function))
     }
 
     pub fn try_create_snapshot(script: Script) -> Result<GolemSnapshot, IsolateCreationError> {
@@ -220,30 +251,25 @@ impl GolemIsolate {
         Ok(snapshot)
     }
 
-    pub fn new(snapshot: &GolemSnapshot) -> Pin<Box<Self>> {
-        let golem = Self::try_new(StartupData::Snapshot(snapshot));
+    pub fn new(snapshot: GolemSnapshot) -> Box<Self> {
+        let golem = Self::try_new(StartupData::Snapshot(&snapshot));
+
         match golem {
-            Ok(g) => g,
+            Ok(g) => {
+                // V8 takes ownership of the snapshot, so to prevent a doublefree, need to forget about the snapshot data
+                forget(snapshot);
+                g
+            }
             Err(e) => panic!("Expected creation of GolemSnapshot to imply that this function has been correctly validated")
         }
     }
-}
 
+    pub fn invoke_main(&mut self) {
+        &self.core_isolate.invoke_function(&self.main_handle);
+    }
 
-fn try_drop(v: Option<impl Drop>) {
-    if let Some(value) = v {
-        drop(value)
-    };
-}
-
-impl Drop for GolemIsolate {
-    fn drop(&mut self) {
-        println!("{}", "Start drop");
-        // self.snapshot.take();
-        try_drop(self.cache_handle.take());
-        println!("{}", "dropped cache handle");
-        try_drop(self.main_handle.take());
-        println!("{}", "dropped main handle");
-        // drop(self.core_isolate);
+    pub async fn await_isolate(self) -> Result<(), ErrBox> {
+        self.core_isolate.await
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use golem;
 use std::time::Instant;
 
-// #[tokio::main]
-fn main() {
+#[tokio::main]
+async fn main() {
     let start_time = Instant::now();
-    { let _ = golem::run_v8(); }
+    golem::run_v8().await;
     let end_time = Instant::now();
     let delta_time = end_time - start_time;
     println!("Run Time: {}ms", delta_time.as_millis());
